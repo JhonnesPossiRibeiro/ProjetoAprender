4ce60a33d4ee743605b276465c1841a6177f9be6
JhonnesPossiRibeiro <jh.possi@gmail.com>
2026-04-07 14:57:01 -0300

    silabas

--- src/components/card/Index.tsx
+++ src/components/card/Index.tsx
@@ -21,7 +21,7 @@ export const CardLetras = ({ titulo,funcao, css, img }: CardProps) => {
           backgroundPosition: "center",
           backgroundRepeat: "no-repeat",
           width: "100%",
-          height: "500px",
+          height: "300px",
         }}
       ></div>
     </div>
