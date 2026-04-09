12bab151223dca1a8a969ff5d89843debe094f9a
JhonnesPossiRibeiro <jh.possi@gmail.com>
2026-04-09 17:53:42 -0300

    scroll

--- src/components/layout/Index.tsx
+++ src/components/layout/Index.tsx
@@ -1,7 +1,7 @@
-
 import { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 import Sidebar from "../sidebar/Index";
+import ScrollToTop from "../scrollToTop/Index";
 
 const Layout = () => {
   const [isExpanded, setIsExpanded] = useState(false);
@@ -21,50 +21,49 @@ const Layout = () => {
   const Menus = [
     {
       label: "Letras",
-      path:"/",
+      path: "/",
       icone: "L",
-      submenus:[]
+      submenus: [],
     },
     {
       label: "Silabas",
-      path:"Silabas",
+      path: "Silabas",
       icone: "S",
-      submenus:[]
+      submenus: [],
     },
     {
       label: "Palavras",
-      path:"Palavras",
+      path: "Palavras",
       icone: "P",
-      submenus:[]
+      submenus: [],
     },
+  ];
 
-  ]
-
-  return(
-    <div
-      style={{
-        display: "flex",
-        height: "100vh",
-        backgroundColor: "#e7e5e5ff"
-      }}
-    >
-      <Sidebar
-        menus={Menus}
-        onExpandChange={setIsExpanded}
-      />
-       <main
+  return (
+    <>
+      <ScrollToTop />
+      <div
         style={{
-          marginLeft: isMobile ? 0 : isExpanded ? 220 : 50,
-          transition: "margin-left 0.3s ease",
-          flex: 1,
-          overflow: "auto",
-          padding: "20px 10px"
+          display: "flex",
+          height: "100vh",
+          backgroundColor: "#e7e5e5ff",
         }}
       >
-        <Outlet />
-      </main>
-    </div>
-  )
-}
+        <Sidebar menus={Menus} onExpandChange={setIsExpanded} />
+        <main
+          style={{
+            marginLeft: isMobile ? 0 : isExpanded ? 220 : 50,
+            transition: "margin-left 0.3s ease",
+            flex: 1,
+            overflow: "auto",
+            padding: "20px 10px",
+          }}
+        >
+          <Outlet />
+        </main>
+      </div>
+    </>
+  );
+};
 
-export default Layout
+export default Layout;
